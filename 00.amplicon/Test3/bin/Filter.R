43d309bbf404645147e123f5ba2f4d6ca559acd4
Hua Zou <zouhua@genomics.cn>
2019-11-04 16:13:38 +0800

    fix Filter.R bugs with numeric parameters

--- 00.amplicon/Test3/bin/Filter.R
+++ 00.amplicon/Test3/bin/Filter.R
@@ -4,16 +4,17 @@
 library(dada2)
 library(dplyr)
 library(argparser)
+library(ggplot2)
 
 # parameter input
 parser <- arg_parser("filter trimmed fq by dada2") %>%
     add_argument("-f", "--fwd", help = "trimmed fq1 directroy") %>%
     add_argument("-r", "--rev", help = "trimmed fq2 directroy") %>%
-    add_argument("-t1", "--trun1", help = "truncLen 1st parameter") %>%
-    add_argument("-t2", "--trun2", help = "truncLen 2nd parameter") %>%
-    add_argument("-me", "--maxEE", help = "maxEE") %>%
-    add_argument("-tq", "--truncQ", help = "truncQ") %>%
-    add_argument("-mn", "--maxN", help = "maxN") %>%
+    add_argument("-t1", "--trun1", type = "integer", help = "truncLen 1st parameter") %>%
+    add_argument("-t2", "--trun2", type = "integer", help = "truncLen 2nd parameter") %>%
+    add_argument("-me", "--maxEE", type = "integer", help = "maxEE") %>%
+    add_argument("-tq", "--truncQ", type = "integer", help = "truncQ") %>%
+    add_argument("-mn", "--maxN", type = "integer", help = "maxN") %>%
     add_argument("-o", "--out", help = "output directory of filtered fq")
     
 args <- parse_args(parser)
@@ -21,16 +22,16 @@ args <- parse_args(parser)
 # prepare for function 
 dir_fq1 <- args$f
 dir_fq2 <- args$r
-trun1   <- args$t1
-trun2   <- args$t2
-maxEE   <- args$me
-trunQ   <- args$tq
-maxN    <- args$mn
+trun1   <- as.numeric(args$t1)
+trun2   <- as.numeric(args$t2)
+maxEE   <- as.numeric(args$me)
+trunQ   <- as.numeric(args$tq)
+maxN    <- as.numeric(args$mn)
 out     <- args$o
 
 # output directory of filtered fq.gz
 filtpathF <- file.path(out, "FWD")
-filtpathR <- file.path(out, "REV") 
+filtpathR <- file.path(out, "REV")
 
 # trimmed fq.gz
 fastqFs <- sort(list.files(dir_fq1))
@@ -40,29 +41,37 @@ if(length(fastqFs) != length(fastqRs)) {
     stop("Forward and reverse files do not match.")
 }
 
+sample.names <- sapply(strsplit(basename(fastqFs), "_"), `[`, 1)
+filtqFs <- paste0(sample.names, "_F_filt.fastq.gz")
+filtFs <- file.path(filtpathF, filtqFs)
+filtqRs <- paste0(sample.names, "_R_filt.fastq.gz")
+filtRs <- file.path(filtpathF, filtqRs)
+
 # filter trimmed.fq.gz
-filter_res <- filterAndTrim(fwd=file.path(dir_fq1, fastqFs),
-                    filt=file.path(filtpathF, fastqFs),
-                    rev=file.path(dir_fq2, fastqRs),
-                    filt.rev=file.path(filtpathR, fastqRs),
-                    truncLen=c(trun1, trun2),
-                    maxEE=maxEE,
-                    truncQ=trunQ,
-                    maxN=maxN,
-                    rm.phix=TRUE,
-                    compress=TRUE,
-                    verbose=TRUE,
-                    multithread=TRUE)
-save(filter_res, paste0(out, "/filter_result.RData"))
+filter_res <- filterAndTrim(
+					fwd         = file.path(dir_fq1, fastqFs),
+                    filt        = filtFs,
+                    rev         = file.path(dir_fq2, fastqRs),
+                    filt.rev    = filtRs,
+                    truncLen    = c(trun1, trun2),
+                    maxEE       = rep(maxEE, 2),
+                    truncQ      = trunQ,
+                    maxN        = maxN,
+                    rm.phix     = TRUE,
+                    compress    = TRUE,
+                    verbose     = TRUE,
+                    multithread = TRUE)
+
+save(filter_res, file=paste0(out, "/filter_result.RData"))
 
 # generate picture file
 plotQP <- function(fq_dir, fq, name){
-  prefix <- paste(out, paste0(name, ".pdf"), sep="/")
+  prefix <- paste(out, paste0(name, ".pdf"), sep = "/")
   plotQualityProfile(file.path(fq_dir, fq))
   ggsave(prefix)
 }
 
-plotQP(fq_dir1, fastqFs, "trim_FWD")
-plotQP(fq_dir2, fastqRs, "trim_REV")
-plotQP(filtpathF, fastqFs, "filt_FWD")
-plotQP(filtpathR, fastqRs, "filt_REV")
+plotQP(dir_fq1, fastqFs, "trim_FWD")
+plotQP(dir_fq2, fastqRs, "trim_REV")
+plotQP(filtpathF, filtqFs, "filt_FWD")
+plotQP(filtpathR, filtqRs, "filt_REV")
