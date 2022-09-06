ed1924cbad3b9f9b675bd2687a5deac087130a07
Hua Zou <zouhua1@outlook.com>
2022-09-06 21:10:24 +0800

    update Download GEOData

--- GEO_download/Download_GEOData_v3.R
+++ GEO_download/Download_GEOData_v3.R
@@ -5,15 +5,15 @@ suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(convert))
 
 option_list <- list(
-  make_option(c("-g", "--GEO"), type="character", default="GSE65858", 
+  make_option(c("-g", "--GEO"), type="character", default="GSE65858",
               help="GEO number", metavar="character"),
-  make_option(c("-p", "--GPL"), type="character",  
+  make_option(c("-p", "--GPL"), type="character",
               help="GPL platform number", metavar="character"),
-  make_option(c("-t", "--type"), type="character",  
-              help="Type of GPL platform", metavar="character"),         
-  make_option(c("-o", "--out"), type="character",  
+  make_option(c("-t", "--type"), type="character",
+              help="Type of GPL platform", metavar="character"),
+  make_option(c("-o", "--out"), type="character",
               help="output", metavar="character")
-); 
+)
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
@@ -33,26 +33,27 @@ gset <- getGEO(GEO = GEO_name,
 phen <- pData(gset[[1]])
 prof <- exprs(gset[[1]])
 
+
 # probe 2 geneid
 gpl <- getGEO(GEO = GPL_number, destdir = dir)
 
 
-if(Array_type == "ILMN"){
+if (Array_type == "ILMN") {
   probe2gene <- Table(gpl) %>%
     dplyr::select(ID, ILMN_Gene) %>%
-    filter(ILMN_Gene != "") %>%
-    setNames(c("ProbeID", "Gene"))  
-}else if(Array_type == "array"){
+    dplyr::filter(ILMN_Gene != "") %>%
+    stats::setNames(c("ProbeID", "Gene"))
+} else if (Array_type == "array") {
   probe2gene <- Table(gpl) %>%
     dplyr::select(all_of(c("ID", "Gene Symbol"))) %>%
-    setNames(c("ProbeID", "Gene")) %>%
-    filter(Gene != "") %>%
-    mutate(Gene=gsub(" /// \\S+", "", Gene))
-}else if(Array_type == "other"){
+    stats::setNames(c("ProbeID", "Gene")) %>%
+    dplyr::filter(Gene != "") %>%
+    dplyr::mutate(Gene = gsub(" /// \\S+", "", Gene))
+} else if (Array_type == "other") {
   library(idmap1)
   probe2gene <- getIDs(tolower(GPL_number)) %>%
     dplyr::select(c("probe_id", "symbol")) %>%
-    setNames(c("ProbeID", "Gene"))  
+    stats::setNames(c("ProbeID", "Gene"))
 }
 
 
@@ -64,15 +65,15 @@ preprocess_Clinical <- function(dataset = phen){
   
   if (length(grep(":ch1$", colnames(dataset))) > 0) {
     dat <- dataset[, c(1, grep(":ch1$", colnames(dataset)))]
-    colnames(dat) <- gsub(":ch1$", "", colnames(dat))    
+    colnames(dat) <- gsub(":ch1$", "", colnames(dat))
   } else {
     dat <- dataset
   }
-
+  
   res <- dat %>%
-    rownames_to_column("bcr_patient_barcode") %>%
-    distinct(bcr_patient_barcode, .keep_all = TRUE) %>%
-    reshape::rename(c(bcr_patient_barcode = "Barcode"))  
+    tibble::rownames_to_column("bcr_patient_barcode") %>%
+    dplyr::distinct(bcr_patient_barcode, .keep_all = TRUE) %>%
+    reshape::rename(c(bcr_patient_barcode = "Barcode"))
   
   return(res)
 }
@@ -82,63 +83,64 @@ phen_post <- preprocess_Clinical(dataset = phen)
 # gene expression profile
 preprocess_profile <- function(dataset,
                                metadata,
-                               annotation=probe2gene){
+                               annotation=probe2gene) {
   
   # dataset=prof
   # metadata=phen_post
   # annotation=probe2gene
   
-  sid <- intersect(colnames(dataset), metadata$Barcode)
+  sid <- dplyr::intersect(colnames(dataset), metadata$Barcode)
   prof_cln <- data.frame(dataset) %>%
-    dplyr::select(all_of(sid)) %>%
-    rownames_to_column("ProbeID") %>%
-    inner_join(annotation, by ="ProbeID")
+    dplyr::select(dplyr::all_of(sid)) %>%
+    tibble::rownames_to_column("ProbeID") %>%
+    dplyr::inner_join(annotation, by = "ProbeID")
   
   # filter features according gene symbol
   idx <- grep("Gene", colnames(prof_cln))
   prof_cln$median <- apply(prof_cln[, -idx], 1, median)
-  prof_cln <- with(prof_cln, prof_cln[order(Gene, median, decreasing = T), ]) 
-  prf_deduplicated <- prof_cln[!duplicated(prof_cln$Gene), ] %>% dplyr::select(-median) 
+  prof_cln <- with(prof_cln, prof_cln[order(Gene, median, decreasing = T), ])
+  prf_deduplicated <- prof_cln[!duplicated(prof_cln$Gene), ] %>%
+    dplyr::select(-median)
   
   # return result: gene symbol
   rownames(prf_deduplicated) <- NULL
-  prof_res <- prf_deduplicated  %>% 
-    dplyr::select(ProbeID, Gene, everything()) %>%
+  prof_res <- prf_deduplicated %>%
+    dplyr::select(ProbeID, Gene, dplyr::everything()) %>%
     dplyr::select(-ProbeID) %>%
-    column_to_rownames("Gene")    
+    tibble::column_to_rownames("Gene")
   
   return(prof_res)
 }
 
-prof_post <- preprocess_profile(dataset=prof, 
-                                metadata=phen_post, 
-                                annotation=probe2gene)
+prof_post <- preprocess_profile(dataset = prof,
+                                metadata = phen_post,
+                                annotation = probe2gene)
 
 # ExpressionSet Object
-get_ExprSet <- function(metadata=phen_post, 
-                        profile=prof_post,
-                        occurrence=0.2){
+get_ExprSet <- function(metadata = phen_post,
+                        profile = prof_post,
+                        occurrence = 0.2) {
   
   # metadata=phen_post
   # profile=prof_post
   # occurrence=0.2
   
   sid <- intersect(metadata$Barcode, colnames(profile))
-  phen_cln <- metadata %>% filter(Barcode%in%sid) %>%
-    column_to_rownames("Barcode")
+  phen_cln <- metadata %>% filter(Barcode %in% sid) %>%
+    tibble::column_to_rownames("Barcode")
   
   prof_cln <- profile %>% dplyr::select(rownames(phen_cln)) %>%
-    rownames_to_column("tmp") %>% 
-    filter(apply(dplyr::select(., -one_of("tmp")), 1, function(x) {
-      sum(x != 0)/length(x)}) > occurrence) %>%
-    column_to_rownames("tmp")
+    tibble::rownames_to_column("tmp") %>%
+    dplyr::filter(apply(dplyr::select(., -one_of("tmp")), 1, function(x) {
+      sum(x != 0) / length(x)}) > occurrence) %>%
+    tibble::column_to_rownames("tmp")
   
-  # determine the right order between profile and phenotype 
-  for(i in 1:ncol(prof_cln)){ 
+  # determine the right order between profile and phenotype
+  for (i in 1:ncol(prof_cln)) {
     if (!(colnames(prof_cln)[i] == rownames(phen_cln)[i])) {
       stop(paste0(i, " Wrong"))
     }
-  }  
+  }
   
   exprs <- as.matrix(prof_cln)
   adf <-  new("AnnotatedDataFrame", data=phen_cln)
@@ -150,13 +152,13 @@ get_ExprSet <- function(metadata=phen_post,
                         url="www.zouhua.top",
                         other=list(notes="Created from text files"))
   expressionSet <- new("ExpressionSet", exprs=exprs,
-                       phenoData=adf, 
+                       phenoData=adf,
                        experimentData=experimentData)
   
   return(expressionSet)
 }
 
-ExprSet_object <- get_ExprSet(metadata=phen_post, 
+ExprSet_object <- get_ExprSet(metadata=phen_post,
                               profile=prof_post,
                               occurrence=0.2)
 
@@ -173,7 +175,7 @@ write.csv(phen_post, file = phen_process, row.names = F)
 
 prof_origin <- paste0(outdir, GEO_name, "_profile_origin.tsv")
 prof_process <- paste0(outdir, GEO_name, "_profile_post.tsv")
-write.table(data.frame(prof) %>% rownames_to_column("GeneID"), 
+write.table(data.frame(prof) %>% rownames_to_column("GeneID"),
             file = prof_origin, row.names = F, quote = F, sep = "\t")
 write.table(prof_post %>% rownames_to_column("GeneID"), 
             file = prof_process, row.names = F, quote = F, sep = "\t")
